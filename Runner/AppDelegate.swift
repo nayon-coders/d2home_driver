a12d6d1d8d6f9b6cb42b3d561e6f61153c751a72
nayon-coders <freelancer4747@gmail.com>
2025-05-25 22:29:18 +0600

    complete project live on google app store

---
+++ Runner/AppDelegate.swift
@@ -0,0 +1,37 @@
+import UIKit
+import Flutter
+import GoogleMaps
+import Firebase
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    GMSServices.provideAPIKey("AIzaSyDhbO5-E45zqrVbPAdJ6mjmbmrDnYyG2eo")
+    GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    if(!UserDefaults.standard.bool(forKey: "Notification")) {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UserDefaults.standard.set(true, forKey: "Notification")
+    }
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        super.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+          let firebaseAuth = Auth.auth()
+            Messaging.messaging().appDidReceiveMessage(userInfo)
+            print(userInfo)
+          if (firebaseAuth.canHandleNotification(userInfo)){
+              completionHandler(.noData)
+              return
+          }
+      }
+
+}
